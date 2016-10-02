83e6547424a42aedfc9caaad682d30d305038a7e
Ed Ellson <eellson@gmail.com>
2016-10-02 22:40:31 +0100

    rm IO

--- lib/brows_ex/parser.ex
+++ lib/brows_ex/parser.ex
@@ -2,7 +2,6 @@ defmodule BrowsEx.Parser do
   @spec parse(binary) :: list
   def parse(input) do
     {:ok, tokens, _} = input |> to_char_list |> :html_lexer.string
-    IO.inspect tokens
     {:ok, list} = tokens |> :html_parser.parse
     list
   end
