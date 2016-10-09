b3ed8d2dbbae627f68c2bbf0c2c28b78d2c503da
Ed Ellson <eellson@gmail.com>
2016-10-09 23:18:24 +0100

    Use floki
    Use floki insted of our own parser; update renderer to handle this.

--- lib/brows_ex/renderer.ex
+++ lib/brows_ex/renderer.ex
@@ -3,34 +3,45 @@ defmodule BrowsEx.Renderer do
   @spec render(tuple) :: tuple
   def render(tree) do
     # IO.inspect tree
-    IO.puts render_node tree
+    tree |> render_node
+
+    # |> IO.puts 
   end
 
   @spec render_text_node(String.t) :: :ok
   def render_text_node(text), do: text
 
-  def render_text_node('<title>', _text), do: ""
-  def render_text_node('<h1>', text), do: text <> "\n" <> underline(text, "=")
-  def render_text_node('<h2>', text), do: text <> "\n" <> underline(text, "-")
-  def render_text_node('<p>', text), do: text <> "\n\n"
-  def render_text_node('<em>', text), do: "_#{text}_"
-  def render_text_node('<strong>', text), do: "*#{text}*"
-  def render_text_node('<small>', text), do: text
-  def render_text_node('<li>', text), do: "* #{text}" <> "\n\n"
-  def render_text_node('<style type="text/css">', text), do: ""
-  def render_text_node(name, text), do: text
+  def render_text_node("title", attributes, _text), do: ""
+  def render_text_node("h1", attributes, text), do: text <> "\n" <> underline(text, "=")
+  def render_text_node("h2", attributes, text), do: text <> "\n" <> underline(text, "-")
+  def render_text_node("p", attributes, text), do: text <> "\n\n"
+  def render_text_node("em", attributes, text), do: "_#{text}_"
+  def render_text_node("strong", attributes, text), do: "*#{text}*"
+  def render_text_node("small", attributes, text), do: text
+  def render_text_node("ul", attributes, text), do: text <> "\n"
+  def render_text_node("ol", attributes, text), do: text <> "\n"
+  def render_text_node("li", attributes, text), do: "* #{text}" <> "\n"
+  def render_text_node("script", attributes, text), do: ""
+  def render_text_node("comment", text), do: ""
+  def render_text_node("div", attributes, text), do: text <> "\n"
+  def render_text_node("td", attributes, text), do: text <> "\n"
+  def render_text_node(name, attributes, text) do
+    # IO.puts "catch all, #{name}"
+    text
+  end
 
   def render_node(text) when (is_binary(text) or is_list(text)) do
     render_text_node(text)
   end
-  def render_node({name, [child]}) when is_binary child do
-    render_text_node(name, child)
+  def render_node({:comment, text}), do: render_text_node("comment", text)
+  def render_node({name, attributes, [child]}) when is_binary child do
+    render_text_node(name, attributes, child)
   end
-  def render_node({name, [child]}) when is_tuple child do
-    render_node(child)
+  def render_node({name, attributes, [child]}) when is_tuple child do
+    render_text_node(name, attributes, render_node(child))
   end
-  def render_node({name, children}) when is_list children do
-    render_node({name, [render_children(children)]})
+  def render_node({name, attributes, children}) when is_list children do
+    render_node({name, attributes, [render_children(children)]})
   end
 
   def render_children([]), do: ""
--- mix.exs
+++ mix.exs
@@ -15,7 +15,7 @@ defmodule BrowsEx.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger, :httpoison]]
+    [applications: [:logger, :httpoison, :floki]]
   end
 
   # Dependencies can be Hex packages:
@@ -28,6 +28,7 @@ defmodule BrowsEx.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:httpoison, "~> 0.9.2"}]
+    [{:httpoison, "~> 0.9.2"},
+     {:floki, "~> 0.10.1"}]
   end
 end
