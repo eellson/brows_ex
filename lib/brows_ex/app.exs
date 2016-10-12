17ce741a883074fcf0cd57fbba01fd2fa9676168
Ed Ellson <eellson@gmail.com>
2016-10-12 18:51:06 +0100

    Refactor App

--- lib/brows_ex/app.exs
+++ lib/brows_ex/app.exs
@@ -1,61 +1,75 @@
 defmodule BrowsEx.App do
   def run(url) do
+    init_ncurses
+    get_and_render_page(url)
+    term_ncurses
+  end
+
+  def init_ncurses do
     ExNcurses.initscr
     ExNcurses.start_color
     ExNcurses.cbreak
-    do_run(url, 1)
-    ExNcurses.endwin
   end
 
-  def do_run(url, highlight) do
-    ExNcurses.clear
-    title = "BrowsEx"
-    print_title("#{title} - #{url}")
-    
-    tree =
+  def term_ncurses, do: ExNcurses.endwin
+  
+  def get_and_render_page(url, highlight \\ 1) do
+    url
+    |> get_tree
+    |> render(highlight)
+  end
+
+  def get_tree(url) do
+    tree = 
       url
       |> BrowsEx.Requester.request
       |> BrowsEx.Parser.parse
       |> BrowsEx.Indexer.index("a")
+      # |> IO.inspect
+
+    {url, tree}
+  end
+
+  def render({url, tree}, highlight \\ 1) do
+    ExNcurses.clear
+    print_title("BrowsEx = #{url}")
 
     tree
     |> BrowsEx.Renderer.render(highlight)
     |> ExNcurses.printw
-    # |> IO.puts
 
-    wait_for_input(url, highlight, tree)
-    # handle_char(?j, url, highlight, tree)
-    # handle_char(?l, url, highlight, tree)
+    ExNcurses.refresh
+
+    wait_for_input({url, tree}, highlight)
   end
 
-  def wait_for_input(url, highlight, tree) do
+  def print_title(string), do: ExNcurses.printw(string)
+
+  def wait_for_input({url, tree}, highlight) do
     ExNcurses.keypad
     ExNcurses.flushinp
 
     char = ExNcurses.getchar
 
-    char |> handle_char(url, highlight, tree)
+    char |> handle_char({url, tree}, highlight)
   end
 
-  def print_title(string), do: ExNcurses.printw(string)
+  def handle_char(?j, {url, tree}, highlight), do: render({url, tree}, highlight + 1)
+  def handle_char(?k, {url, tree}, highlight), do: render({url, tree}, highlight - 1)
+  def handle_char(?l, {url, tree}, highlight), do: do_click({url, tree}, highlight)
+  def handle_char(?q, {url, tree}, highlight), do: nil
+  def handle_char(_, {url, tree}, highlight), do: wait_for_input({url, tree}, highlight)
 
-  def handle_char(?j, url, highlight, tree), do: do_run(url, highlight + 1)
-  def handle_char(?k, url, highlight, tree), do: do_run(url, highlight - 1)
-  def handle_char(?l, url, highlight, tree), do: do_click(highlight, tree, url) # TODO wrong index
-  def handle_char(?q, url, highlight, tree), do: nil
-  def handle_char(_, url, highlight, tree), do: wait_for_input(url, highlight, tree)
-
-  def do_click(index, tree, current_url) do
-    url =
-      tree
-      # |> IO.inspect
-      |> Floki.find("[brows_ex_index=#{index}]")
-      # |> IO.inspect
-      |> List.first
-      |> Floki.attribute("href")
-      |> List.first
-      |> BrowsEx.Requester.transform_url(current_url)
-      |> do_run(1)
+  def do_click({current_url, tree}, index) do
+    tree
+    # |> IO.inspect
+    |> Floki.find("[brows_ex_index=#{index}]")
+    # |> IO.inspect
+    |> List.first
+    |> Floki.attribute("href")
+    |> List.first
+    |> BrowsEx.Requester.transform_url(current_url)
+    |> get_and_render_page
   end
 end
 
