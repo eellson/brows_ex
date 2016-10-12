0b5cb21147883152a2b3b92d044e52f570ee990d
Ed Ellson <eellson@gmail.com>
2016-10-13 00:11:55 +0100

    pretty formatting for colors

--- lib/brows_ex/renderer.ex
+++ lib/brows_ex/renderer.ex
@@ -8,9 +8,9 @@ defmodule BrowsEx.Renderer do
 
   def render_node({"head", attributes, children}, highlight), do: nil
   def render_node({"a", attributes, children}, highlight) do
-    ExNcurses.printw("[")
-    render_node(children, highlight)
-    ExNcurses.printw("]")
+    attributes
+    |> get_index
+    |> render_link(highlight, children)
   end
   def render_node({name, attributes, children}, highlight) do
     render_node(children, highlight)
@@ -24,4 +24,23 @@ defmodule BrowsEx.Renderer do
     # |> IO.puts
   end
   def render_node(other, highlight), do: other
+
+  def get_index(attributes) do
+    {_, index} = attributes |> Enum.find(&({"brows_ex_index", index} = &1))
+
+    index
+  end
+
+  def render_link(highlight, highlight, children) do
+    ExNcurses.init_pair(1, ExNcurses.clr(:RED), ExNcurses.clr(:BLACK))
+    ExNcurses.attron(1)
+    render_link(children, highlight)
+    ExNcurses.attroff(1)
+  end
+  def render_link(index, highlight, children), do: render_link(children, highlight)
+  def render_link(children, highlight) do
+    ExNcurses.printw("[")
+    render_node(children, highlight)
+    ExNcurses.printw("]")
+  end
 end
